0801fe6cf4543d7c78606ffc313917a02983a11c
Rik Huygen <rik.huygen@kuleuven.be>
2025-02-18 13:48:43 +0100

    hexapod puna fixed and unit tests added

--- projects/generic/symetrie-hexapod/src/symetrie_hexapod/cgse_services.py
+++ projects/generic/symetrie-hexapod/src/symetrie_hexapod/cgse_services.py
@@ -1,5 +1,8 @@
-# An example plugin for the `cgse {start,stop,status} service` command from `cgse-core`.
-#
+import subprocess
+import sys
+from pathlib import Path
+from typing import Annotated
+
 import rich
 import typer
 
@@ -11,24 +14,43 @@ puna = typer.Typer(
 
 
 @puna.command(name="start")
-def start_puna():
-    """Start the PUNA service."""
-    rich.print("Starting service PUNA")
+def start_puna(
+        simulator: Annotated[
+            bool,
+            typer.Option("--simulator", "--sim", help="start the hexapod PUNA simulator in the background")
+        ] = False
+):
+    """Start the PUNA hexapod control server."""
+    rich.print(f"Starting the PUNA hexapod control server – {simulator = }")
+
+    out = open(Path('~/.puna_cs.start.out').expanduser(), 'w')
+
+    cmd = [sys.executable, '-m', 'egse.hexapod.symetrie.puna_cs', 'start']
+    if simulator:
+        cmd.append("--simulator")
+
+    subprocess.Popen(
+        cmd,
+        stdout=out, stderr=out, stdin=subprocess.DEVNULL,
+        close_fds=True
+    )
 
 
 @puna.command(name="stop")
 def stop_puna():
     """Stop the PUNA service."""
-    rich.print("Terminating service PUNA")
+    rich.print("Terminating hexapod PUNA control server..")
+
+    out = open(Path('~/.puna_cs.stop.out').expanduser(), 'w')
+
+    subprocess.Popen(
+        [sys.executable, '-m', 'egse.hexapod.symetrie.puna_cs', 'stop'],
+        stdout=out, stderr=out, stdin=subprocess.DEVNULL,
+        close_fds=True
+    )
 
 
 @puna.command(name="status")
 def status_puna():
     """Print status information on the PUNA service."""
     rich.print("Printing the status of PUNA")
-
-
-@puna.command(name="start-simulator")
-def start_puna_sim():
-    """Start the PUNA Simulator."""
-    rich.print("Starting service PUNA Simulator")
