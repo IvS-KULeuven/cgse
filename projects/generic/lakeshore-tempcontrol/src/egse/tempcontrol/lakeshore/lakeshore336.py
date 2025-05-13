250fbe42c666816eb10fb17cae907652310e29c4
Sara Regibo <sara@ster.kuleuven.be>
2025-05-13 10:57:03 +0200

    LakeShore336 controller

--- projects/generic/lakeshore-tempcontrol/src/egse/tempcontrol/lakeshore/lakeshore336.py
+++ projects/generic/lakeshore-tempcontrol/src/egse/tempcontrol/lakeshore/lakeshore336.py
@@ -1,4 +1,43 @@
 class LakeShore336Proxy(Proxy, Lakeshore336Interface):
+class LakeShore336Interface(DeviceInterface):
+
+    def __init__(self, device_id: str):
+        """ Initialisation of a LakeShore336 interface.
+
+        Args:
+            device_id (str): Device identifier
+        """
+
+        super().__init__()
+
+        self.device_id = device_id
+
+class LakeShore336Controller(LakeShore336Interface):
+
+    def __init__(self, device_id: str):
+        super().__init__(device_id)
+
+        self.lakeshore = self.transport = LakeShore336EthernetInterface(device_id)
+
+    def is_simulator(self) -> bool:
+
+        return False
+
+    def is_connected(self) -> bool:
+
+        return self.lakeshore.is_connected()
+
+    def connect(self):
+
+        self.lakeshore.connect()
+
+    def disconnect(self):
+
+        self.lakeshore.disconnect()
+
+    def reconnect(self):
+
+        self.lakeshore.reconnect()
     """ Proxy to connect to a LakeShore 336 Control Server """
 
     def __init__(self, device_id: str):
