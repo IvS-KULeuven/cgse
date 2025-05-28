cac0d06f4817d78faec937b07e5df6c61b6bfed7
Sara Regibo <sara@ster.kuleuven.be>
2025-05-28 10:46:38 +0200

    Corrected `cgse_explore.py` for LakeShore336

--- projects/generic/lakeshore-tempcontrol/src/lakeshore_tempcontrol/cgse_explore.py
+++ projects/generic/lakeshore-tempcontrol/src/lakeshore_tempcontrol/cgse_explore.py
@@ -7,4 +7,4 @@ from egse.process import ps_egrep
 
 def show_processes():
     """Show the lines from the `ps -ef` command that match processes from this package."""
-    return ps_egrep("(lsci336)_(ui|cs|sim)")
+    return ps_egrep("(lakeshore336)_(ui|cs|sim)")
