af19c4dd1eb6e5b6ef07663c0f7f8c5c96631439
Rik Huygen <rik.huygen@kuleuven.be>
2025-08-29 08:28:19 +0200

    added some basic unit tests

---
+++ libs/cgse-core/tests/script_subscribe_to_notifyhub.py
@@ -0,0 +1,26 @@
+from egse.log import logger
+from egse.notifyhub.services import EventSubscriber
+
+subscriber = EventSubscriber([""])
+subscriber.connect()
+
+
+def handle_simple_event(event_data: dict):
+    logger.info(f"----> {event_data=}")
+
+def handle_single_event(event_data: dict):
+    logger.info(f"----> {event_data=}")
+
+subscriber.register_handler("simple-event", handle_simple_event)
+subscriber.register_handler("single-event", handle_single_event)
+
+while True:
+    try:
+        if subscriber.poll():
+            logger.info("Event spotted...")
+            subscriber.handle_event()
+    except KeyboardInterrupt:
+        logger.info("Caught KeyboardInterrupt, terminating...")
+        break
+
+subscriber.close()
