d8a47f09c0b166bf13f848e47c7a446f6054bf92
Sara Regibo <sara@ster.kuleuven.be>
2025-09-04 14:59:04 +0200

    Updated `filter_procs` for Ariel TCU

--- projects/ariel/ariel-tcu/src/ariel_tcu/cgse_explore.py
+++ projects/ariel/ariel-tcu/src/ariel_tcu/cgse_explore.py
@@ -10,7 +10,7 @@ from egse.process import ProcessInfo, get_processes
 def show_processes():
 
     def filter_procs(pi: ProcessInfo):
-        pattern = r"tcu_(ui|cs|sim)"
+        pattern = r"tcu_(ui|cs)"
 
         return re.search(pattern, pi.command)
 
