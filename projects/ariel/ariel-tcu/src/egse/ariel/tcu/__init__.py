9ed59e2771447d91fed6e8fc6967af486c3a2464
Sara Regibo <sara@ster.kuleuven.be>
2025-10-28 16:31:09 +0100

    Moved `TcuMode` + `MotorState`

--- projects/ariel/ariel-tcu/src/egse/ariel/tcu/__init__.py
+++ projects/ariel/ariel-tcu/src/egse/ariel/tcu/__init__.py
@@ -1,3 +1,4 @@
+from enum import IntEnum
 from pathlib import Path
 
 from egse.settings import Settings
@@ -15,3 +16,34 @@ SERVICE_TYPE = settings.get("SERVICE_TYPE", "tcu_cs")
 STORAGE_MNEMONIC = settings.get("STORAGE_MNEMONIC", "TCU")
 
 PROXY_TIMEOUT = 10
+
+
+class TcuMode(IntEnum):
+    """Ariel TCU operating modes.
+
+    The different TCU modes are:
+    - IDLE: Waiting for commands, minimum power consumption,
+    - BASE: HK + TSM circuitry on,
+    - CALIBRATION: HK + TSM + M2MD circuitry on.
+    """
+
+    # Adopted from Vladimiro's code
+
+    IDLE = 0x0000  # Waiting for commands, minimum power consumption
+    BASE = 0x0001  # HK + TSM circuitry on
+    CALIBRATION = 0x0003  # HK + TSM + M2MD circuitry on
+
+
+class MotorState(IntEnum):
+    """State of the M2MD motors.
+
+    The different motor states are:
+    - STANDBY: No motion,
+    - OPERATION: Motor moving.
+    """
+
+    # Adopted from Vladimiro's code
+    # RD01 -> Sect. 5.1
+
+    STANDBY = 0x0001  # No motion
+    OPERATION = 0x0010  # Motor moving
