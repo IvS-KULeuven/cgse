919bc293f89b84130ec5764bd8d2a9959b12526b
Sara Regibo <sara@ster.kuleuven.be>
2026-01-23 16:45:34 +0100

    Implementation of Digilent MEASURpoint
    This is part of #225.

--- projects/generic/digilent/src/egse/digilent/measurpoint/dt8874/dt8874.py
+++ projects/generic/digilent/src/egse/digilent/measurpoint/dt8874/dt8874.py
@@ -2,10 +2,9 @@
 
 import logging
 
-from egse.device import DeviceInterface
-from egse.digilent.digilent import DigilentInterface
+from egse.digilent.digilent import DigilentController, DigilentInterface
 from egse.digilent.measurpoint.dt8874 import COMMANDING_PORT, PROTOCOL, HOSTNAME, SERVICE_TYPE, PROXY_TIMEOUT
-from egse.mixin import dynamic_command, CommandType, DynamicCommandMixin, add_lf
+from egse.digilent.measurpoint.dt8874.dt8874_devif import Dt8874EthernetInterface
 from egse.proxy import DynamicProxy
 from egse.registry.client import RegistryClient
 from egse.zmq_ser import connect_address
@@ -13,82 +12,29 @@ from egse.zmq_ser import connect_address
 LOGGER = logging.getLogger("egse.digilent.measurpoint.dt8874")
 
 
+class Dt8874Interface(DigilentInterface):
+    """Base class for Digilent MEASURpoint DT8874 instruments."""
 
 
-class Dt8874Controller(DigilentInterface, DynamicCommandMixin):
+class Dt8874Controller(DigilentController, DigilentInterface):
     def __init__(self):
         """Initialisation of an Ariel TCU controller."""
 
         super().__init__()
-        self.transport = self.dt8874 = Dt8874DeviceInterface()
+
+        self.transport = self.dt8874 = Dt8874EthernetInterface()
 
 
-    # noinspection PyMethodMayBeStatic
-    def is_simulator(self):
-        return False
-
-    def is_connected(self):
-        """Checks whether the connection to the Digilent MEASURpoint DT8874 is open.
-
-        Returns:
-            True if the serial port to the Digilent MEASURpoint DT8874 is open; False otherwise.
-        """
-
-        return self.dt8874.is_connected()
-
-    def connect(self):
-        """Opens the connection to the Digilent MEASURpoint DT8874.
-
-        Raises:
-            Dt8874Error: When the connection could not be opened.
-        """
-
-        self.dt8874.connect()
-
-    def disconnect(self):
-        """Closes the connection to the Digilent MEASURpoint DT8874.
-
-        Raises:
-            Dt8874Error: When the connection could not be closed.
-        """
-
-        self.dt8874.disconnect()
-
-    def reconnect(self):
-        """Re-connects to the Digilent MEASURpoint DT8874."""
-
-        self.transport.reconnect()
-
-
-class Dt8874Simulator(DigilentInterface):
+class Dt8874Simulator(Dt8874Interface):
     def __init__(self):
         """Initialisation of a Digilent MEASURpoint DT8874 simulator."""
 
         super().__init__()
 
-        self._is_connected = True
 
-    # noinspection PyMethodMayBeStatic
-    def is_simulator(self):
-        return True
-
-    # noinspection PyMethodMayBeStatic
-    def is_connected(self):
-        return self._is_connected
-
-    def connect(self):
-        self._is_connected = True
-
-    def disconnect(self):
-        self._is_connected = False
-
-    def reconnect(self):
-        self._is_connected = True
-
-
-class Dt8874Proxy(DynamicProxy, DigilentInterface):
+class Dt8874Proxy(DynamicProxy, Dt8874Interface):
     """
-    The TDt8874Proxy class is used to connect to the Digilent MEASURpoint DT8874 Control Server and send commands to
+    The Dt8874Proxy class is used to connect to the Digilent MEASURpoint DT8874 Control Server and send commands to
     the Digilent MEASURpoint DT8874 Hardware Controller remotely.
     """
 
