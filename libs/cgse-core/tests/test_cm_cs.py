b6cda9b748bb7db35e2989d31236ea2aca89a35a
Rik Huygen <rik.huygen@kuleuven.be>
2026-03-17 11:40:27 +0100

    Fix HDF5 handling in StorageController and add integration test for observation lifecycle

--- libs/cgse-core/tests/test_cm_cs.py
+++ libs/cgse-core/tests/test_cm_cs.py
@@ -1,7 +1,10 @@
+import time
 from typing import List
 
 import pytest
 import rich
+
+from egse.log import logger
 
 from egse.confman import ConfigurationManagerProxy
 from egse.confman import is_configuration_manager_active
@@ -56,3 +59,25 @@ def test_get_setup():
 
         setup = cm.get_setup()
         assert setup.get_id() == "00000"
+
+
+@pytest.mark.skipif(not is_configuration_manager_active(), reason="core service cm_cs not running")
+def test_start_observation():
+    from egse.obsid import ObservationIdentifier
+
+    with ConfigurationManagerProxy() as cm:
+        setup = cm.load_setup(setup_id=0)
+        assert setup.get_id() == "00000"
+
+        response = cm.start_observation({"description": "Test observation"})
+
+        logger.info(f"Observation started with response: {response}, {response.return_code}")
+        assert response.message.startswith("Returning the OBSID")
+        assert isinstance(response.return_code, ObservationIdentifier)
+
+        time.sleep(1.0)
+
+        response = cm.end_observation()
+        logger.info(f"Observation ended with response: {response}")
+        assert response.message == "Successfully ended the observation."
+        assert response.return_code is None
